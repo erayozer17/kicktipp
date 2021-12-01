3217f1591ba11d5574aebd821b61963181e1d506
Eray Özer <erayozer91@protonmail.com>
2021-12-01 16:06:54 +0100

    first commit

---
+++ src/main/java/com/erayozer/kicktipp/KicktippApplication.java
@@ -0,0 +1,13 @@
+package com.erayozer.kicktipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KicktippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KicktippApplication.class, args);
+	}
+
+}
