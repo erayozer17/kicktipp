2b1fdc90f0219ae8b6c8a743209fbd7d892afaa9
Eray Özer <erayozer91@protonmail.com>
2021-12-03 00:58:57 +0100

    add worker impl

--- src/main/java/com/erayozer/kicktipp/KicktippApplication.java
+++ src/main/java/com/erayozer/kicktipp/KicktippApplication.java
@@ -1,13 +1,28 @@
 package com.erayozer.kicktipp;
 
+import com.erayozer.kicktipp.worker.WorkerService;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.scheduling.cron.Cron;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class KicktippApplication {
+
+	@Autowired
+	private JobScheduler jobScheduler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KicktippApplication.class, args);
 	}
 
+	@PostConstruct
+	public void scheduleRecurrent(){
+		jobScheduler.<WorkerService>scheduleRecurrently("score-retriever", Cron.every5minutes(), x -> x.executeSampleJob("yeni job basladi"));
+		//jobScheduler.delete("score-retriever"); // this stops the job above (pay attention to the id)
+		// https://www.api-football.com/documentation-v3#operation/get-fixtures
+	}
 }
